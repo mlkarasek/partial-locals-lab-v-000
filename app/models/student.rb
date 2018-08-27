efe54237efb3d07e811b2d715f6a663fb5a696c5
Monica  Karasek <mlkarasek@gmail.com>
2018-08-27 20:36:38 +0000

    automatically backed up by learn

---
+++ app/models/student.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  hometown   :string
+#  birthday   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Student < ActiveRecord::Base
+  has_many :classroom_students
+  has_many :classrooms, through: :classroom_students
+
+
+  def self.search(student_name)
+      if student_name.empty?
+          @students = self.all
+      else
+         self.all.find_all do |student|
+              student.name.downcase.include?(student_name)
+          end
+      end
+   end
+end
